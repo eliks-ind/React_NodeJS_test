711bbabdfe208818de43e9077425ddff58091b60
eliks-ind <20fantomas09@gmail.com>
2024-04-13 21:20:30 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api", (req, res) => {
-    res.send("Ilgar pidor");
+    res.send("Uraaaa! It works!");
 });
 
-app.listen(5000, () => {console.log("shit is working at port 5000")});
+app.listen(5000, () => {console.log("server is working at port 5000")});
